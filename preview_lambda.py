47241776fe8a3c49033796ff6574c308f45aa80f
msherma9 <mason.sherman07@gmail.com>
2024-07-23 19:44:51 -0500

    Update preview_lambda.py

--- preview_lambda.py
+++ preview_lambda.py
@@ -3,8 +3,8 @@ import boto3
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = 'bento-kendra-test-bucket'
-    file_name = 'users.csv'
+    bucket_name = 'bentodata'
+    file_name = 'fileOverview.csv'
 
     response = s3.get_object(Bucket=bucket_name, Key=file_name)
     file_content = response["Body"].read().decode('utf-8')
